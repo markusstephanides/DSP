815acb6fdf0328556f534a0b3adf7490f3a65e52
Digot <maste@live.at>
2017-03-19 16:31:06 +0100

    Quick update

--- DSP/InputChannel.cpp
+++ DSP/InputChannel.cpp
@@ -5,7 +5,7 @@
 
 InputChannel::InputChannel(const char* name): Channel(name)
 {
-	output_ = AnalogAudioOutput(1);
+	output_ = static_cast<AudioOutput>(AnalogAudioOutput(1));
 }
 
 void InputChannel::read(byte audioData[])

103fa4c8b8d357c120be6a53a849512aadfcf591
Digot <maste@live.at>
2017-03-19 16:26:18 +0100

    Quick update

--- DSP/InputChannel.cpp
+++ DSP/InputChannel.cpp
@@ -5,7 +5,7 @@
 
 InputChannel::InputChannel(const char* name): Channel(name)
 {
-	//output_ = AnalogAudioOutput(1);
+	output_ = AnalogAudioOutput(1);
 }
 
 void InputChannel::read(byte audioData[])
@@ -18,6 +18,6 @@ void InputChannel::read(byte audioData[])
 	}
 
 	printf("Sum: %i\n", sum);
-	//output_.write(audioData);
+	output_.write(audioData);
 
 }

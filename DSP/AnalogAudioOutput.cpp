29850c64a705ce887366ca6532cea454615f5d95
Digot <maste@live.at>
2017-03-19 15:06:13 +0100

    Quick update

--- DSP/AnalogAudioOutput.cpp
+++ DSP/AnalogAudioOutput.cpp
@@ -7,35 +7,35 @@ AnalogAudioOutput::AnalogAudioOutput(int hwIndex)
 {
 	this->hwIndex = hwIndex;
 
-	this->stream = AnalogAudioSystem::openOutputStream(1, patestCallback, this);
+	this->stream = AnalogAudioSystem::openOutputStream(1, nullptr, this);
 }
 
-int AnalogAudioOutput::patestCallback(const void *inputBuffer, void *outputBuffer,
-	unsigned long framesPerBuffer,
-	const PaStreamCallbackTimeInfo* timeInfo,
-	PaStreamCallbackFlags statusFlags,
-	void *userData)
-{
-	float *out = static_cast<float*>(outputBuffer);
-	unsigned long i;
-
-	(void)timeInfo; /* Prevent unused variable warnings. */
-	(void)statusFlags;
-	(void)inputBuffer;
-
-
-	//for (i = 0; i < framesPerBuffer; i++)
-	//{
-	//	*out++ = data->sine[data->left_phase];  /* left */
-	//	*out++ = data->sine[data->right_phase];  /* right */
-	//	data->left_phase += 1;
-	//	if (data->left_phase >= TABLE_SIZE) data->left_phase -= TABLE_SIZE;
-	//	data->right_phase += 3; /* higher pitch so we can distinguish left and right. */
-	//	if (data->right_phase >= TABLE_SIZE) data->right_phase -= TABLE_SIZE;
-	//}
-
-	return paContinue;
-}
+//int AnalogAudioOutput::patestCallback(const void *inputBuffer, void *outputBuffer,
+//	unsigned long framesPerBuffer,
+//	const PaStreamCallbackTimeInfo* timeInfo,
+//	PaStreamCallbackFlags statusFlags,
+//	void *userData)
+//{
+//	float *out = static_cast<float*>(outputBuffer);
+//	unsigned long i;
+//
+//	(void)timeInfo; /* Prevent unused variable warnings. */
+//	(void)statusFlags;
+//	(void)inputBuffer;
+//
+//
+//	//for (i = 0; i < framesPerBuffer; i++)
+//	//{
+//	//	*out++ = data->sine[data->left_phase];  /* left */
+//	//	*out++ = data->sine[data->right_phase];  /* right */
+//	//	data->left_phase += 1;
+//	//	if (data->left_phase >= TABLE_SIZE) data->left_phase -= TABLE_SIZE;
+//	//	data->right_phase += 3; /* higher pitch so we can distinguish left and right. */
+//	//	if (data->right_phase >= TABLE_SIZE) data->right_phase -= TABLE_SIZE;
+//	//}
+//
+//	return paContinue;
+//}
 
 AnalogAudioOutput::~AnalogAudioOutput()
 {

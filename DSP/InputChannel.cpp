1082b0822bfa31429ea7488d0a2d1e18183bfa74
Digot <maste@live.at>
2017-03-19 02:21:14 +0100

    Quick update

--- DSP/InputChannel.cpp
+++ DSP/InputChannel.cpp
@@ -12,10 +12,6 @@ void InputChannel::read(byte audioData[])
 		sum += audioData[i];
 	}
 	Logger::log("7");
-	char* str = nullptr;
 	Logger::log("7.1");
-	snprintf(str, sizeof(str), "%i\n", sum);
-	Logger::log("8");
-	Logger::log(str);
-	Logger::log("9");
+	printf("%i\n", sum);
 }

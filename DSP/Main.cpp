7d0adb176f98a063cf38ab0a0ab4a5cf97fde516
Digot <maste@live.at>
2017-03-19 15:44:25 +0100

    Quick update

--- DSP/Main.cpp
+++ DSP/Main.cpp
@@ -13,12 +13,15 @@ int main() {
 	//AnalogAudioSystem::init();
 
 	// to test the audan interface, we create a digital audio input along with some channels
+	Logger::log("1");
 	DigitalAudioInput* digital_audio_input = UDPClient::registerDigitalInput(10);
+	Logger::log("2");
 	InputChannel channel = InputChannel();
+	Logger::log("3");
 	digital_audio_input->addListeningChannel(channel);
-
+	Logger::log("4");
 	UDPClient::start();
-
+	Logger::log("5");
 	Logger::log("Waiting for user input...");
 	getchar();
 	AnalogAudioSystem::shutdown();
